f7f9f7e89d9d3a31ff7ba4b37039684cd20aacbd
arielsardinha <odararepresentacoes@outlook.com.br>
2022-04-11 17:46:28 -0300

    projeto parte 4

--- lib/projeto/index.dart
+++ lib/projeto/index.dart
@@ -49,13 +49,54 @@ class CarrinhoDeCompras extends StatelessWidget {
                 crossAxisCount: 3,
                 crossAxisSpacing: 10,
                 mainAxisSpacing: 10,
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                children: List.generate(
-                  9,
-                  (index) => Container(
-                    color: Colors.white,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                children: [
+                  card(
+                    img: 'assets/images/Frame.png',
+                    nome: 'Cereja',
+                    valor: '9,99',
                   ),
-                ),
+                  card(
+                    img: 'assets/images/Frame1.png',
+                    nome: 'Pera',
+                    valor: '1,99',
+                  ),
+                  card(
+                    img: 'assets/images/Frame2.png',
+                    nome: 'Maçã Verde',
+                    valor: '2,99',
+                  ),
+                  card(
+                    img: 'assets/images/Frame3.png',
+                    nome: 'Framboesa',
+                    valor: '7,99',
+                  ),
+                  card(
+                    img: 'assets/images/Frame4.png',
+                    nome: 'Kiwi',
+                    valor: '6,99',
+                  ),
+                  card(
+                    img: 'assets/images/Frame5.png',
+                    nome: 'Melancia',
+                    valor: '5,99',
+                  ),
+                  card(
+                    img: 'assets/images/Frame6.png',
+                    nome: 'Uva',
+                    valor: '3,99',
+                  ),
+                  card(
+                    img: 'assets/images/Frame7.png',
+                    nome: 'Laranja',
+                    valor: '2,99',
+                  ),
+                  card(
+                    img: 'assets/images/Frame8.png',
+                    nome: 'Morango',
+                    valor: '9,99',
+                  )
+                ],
               ),
             ),
             SizedBox(
@@ -76,4 +117,34 @@ class CarrinhoDeCompras extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget card(
+    {required String img, required String nome, required String valor}) {
+  return Container(
+    decoration: const BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.all(
+        Radius.circular(10),
+      ),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Image.asset(img),
+        Text(
+          nome,
+          style: const TextStyle(color: Colors.blue),
+        ),
+        Text(
+          'R\$$valor',
+          style: const TextStyle(
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        )
+      ],
+    ),
+  );
 }
